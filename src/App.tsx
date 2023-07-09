4101fd23badebc71e767bb9765dffad64fc572a3
Lorenzo Sfienti <lorenzo.sfienti@gmail.com>
2023-07-09 10:15:22 +0200

    First release

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+function App() {
+  return (
+    <Heading level={1} textAlign="center">Hello world</Heading>
+  );
+}
+
+export default App;
